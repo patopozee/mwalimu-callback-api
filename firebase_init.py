5c2a917b43d08675d833180087034b69d91e9317
patopozee <mugopatrick13@gmail.com>
2026-07-23 12:54:19 +0300

    update files

--- firebase_init.py
+++ firebase_init.py
@@ -1,23 +1,11 @@
-import json
 import firebase_admin
-import streamlit as st
-
 from firebase_admin import credentials, firestore
 
-
 def initialize_firebase():
-
+    # Only initialize if the app hasn't been initialized yet
     if not firebase_admin._apps:
-
-        raw_json = st.secrets["firebase"]["service_account_json"]
-
-        cred_dict = json.loads(raw_json)
-
-        cred = credentials.Certificate(cred_dict)
-
-        firebase_admin.initialize_app(cred)
-
+        # Use Application Default Credentials (ADC) provided by Cloud Run automatically
+        firebase_admin.initialize_app()
     return firestore.client()
-
 
 db = initialize_firebase()
